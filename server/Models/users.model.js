c46f42402a7685c8f59dd0f4ca56981271d4728b
SachaFoucard <sachafoucard8@gmail.com>
2023-06-14 00:15:02 +0300

    updated
    Add into ModelUser a function to Register a new user and his route  into Route User //
    
    Add into ModelUser a function to Login a new user and his route into Route User //

--- server/Models/users.model.js
+++ server/Models/users.model.js
@@ -1,27 +1,47 @@
-const DB = require('../utils/db')
+const DB = require('../utils/db');
+const bcrypt = require('bcrypt');
 
 class UserModel {
-    _id;
-    name;
-    mail;
-    password;
-    phone;
-    gender;
-    country;
+  _id;
+  name;
+  mail;
+  password;
+  phone;
+  gender;
+  country;
 
-    constructor(name, mail, password) {
-        this.name = name;
-        this.mail = mail;
-        this.password = password;
+  constructor(name, mail, password) {
+    this.name = name;
+    this.mail = mail;
+    this.password = password;
+  }
+
+  static async Register(name, mail, password1) {
+    try {
+      let password = await bcrypt.hash(password1, 10);
+      let user = { name, mail, password }
+      console.log(user);
+      if (name && mail && password) {
+        return await new DB().Insert('users', { name, mail, password });
+      }
+      if (!name || !mail || !password) {
+        throw new Error('One or more fields are empty');
+      }
+    } catch (error) {
+      return error
+    }
+  }
+  static async Login(mail, password) {
+    let query = { mail: mail }
+    let user = await new DB().FindOne("users", query);
+    if (!user || !(await bcrypt.compare(password, user.password)))
+      return null;
+    else {
+      return {
+        user
+      };
     }
 
-    static async Register(name,mail,password) {
-      try {
-       let user = {name,mail,password}
-        return await new DB().Insert('users', {user});
-      } catch (error) {
-        return error
-      }       
-    }
+  }
 }
 module.exports = UserModel
--- server/Routes/users.route.js
+++ server/Routes/users.route.js
@@ -2,14 +2,32 @@ const userRoutes = require('express').Router()
 const UserModel = require('../Models/users.model')
 
 userRoutes.post('/register', async (req, res) => {
-    try {
-        let {name,mail,password} = req.body;
-   
-        let user = await UserModel.Register(name,mail,password);
-        res.status(201).json(user)
-    } catch (error) {
-        res.status(500).json({ error })
+  try {
+    let { name, mail, password } = req.body;
+
+    if (!name || !mail || !password) {
+      return res.status(400).json({ error: 'Check your field, One of them are empty' });
     }
+    if (name || mail || password) {
+      let user = await UserModel.Register(name, mail, password);
+      res.status(201).json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+userRoutes.post('/login', async (req, res) => {
+let {mail,password} = req.body;
+
+let user = await UserModel.Login(mail,password)
+if(!user){
+  res.status(401).json({message : 'user not found'})
+}
+else{
+  res.status(200).json(user)
+}
+
 })
 
 module.exports = userRoutes
