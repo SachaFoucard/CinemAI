db9654f89cc6013546fae52197f8fe77d93d9efd
SachaFoucard <sachafoucard8@gmail.com>
2023-06-30 19:25:35 +0300

    Update UserContext.jsx

--- client/my-app/src/context/UserContext.jsx
+++ client/my-app/src/context/UserContext.jsx
@@ -37,7 +37,7 @@ const UserContextProvider = ({ children }) => {
     };
 
     const SetUpGenre = async (navigation) => {
-        let response = await fetch('http://localhost:8000/api/updateGenre',{
+        let response = await fetch('https://cinemai.onrender.com/api/updateGenre',{
             method:'POST',
             headers:{
                 'Content-Type': 'application/json'
