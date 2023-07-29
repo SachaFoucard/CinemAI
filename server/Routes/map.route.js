d8bc01fa3a8d21b84bdd337c58860b77ae3cd8cc
SachaFoucard <sachafoucard8@gmail.com>
2023-07-29 22:14:50 +0300

    Update map.route.js

--- server/Routes/map.route.js
+++ server/Routes/map.route.js
@@ -3,10 +3,13 @@ const MapModel = require('../Models/map.models.js')
 
 mapRoutes.post('/findCinema', async (req, res) => {
     let { lat, long } = req.body
-    let places = await MapModel.MoreClosestPlace(lat, long);
+    let places = await MapModel.MoreClosestPlaces(lat, long);
     return res.status(201).json(places)
-    // calculateDistance(long, lat,)
 })
 
+mapRoutes.get('/findallCinema', async (req, res) => {
+    let places = await MapModel.AllPlaces();
+    return res.status(201).json(places)
+})
 
 module.exports = mapRoutes;
