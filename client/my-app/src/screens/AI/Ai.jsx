ede7710224782fe12ca979e2ab6b1e230a4fdd05
SachaFoucard <sachafoucard8@gmail.com>
2023-08-05 22:58:16 +0300

    update

--- client/my-app/src/screens/AI/Ai.jsx
+++ client/my-app/src/screens/AI/Ai.jsx
@@ -1,85 +1,116 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import aiData from '../../data/ai.json';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Ai = () => {
-  const [displayText, setDisplayText] = useState('');
+  const [allMessages, setAllMessages] = useState([]);
+  const [textToType, setTextToType] = useState([]);
   const typingSpeed = 50; // Adjust typing speed as needed
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [count, setCount] = useState(0);
-  const [ai, setAi] = useState([]);
-  const [userInput, setUserInput] = useState('');
+  const [input, setInput] = useState('');
+  const inputRef = useRef(null);
 
-  const simulateTyping = () => {
-    if (currentIndex < ai[count]?.text.length) {
-      setDisplayText(ai[count]?.text.slice(0, currentIndex + 1));
+  const simulateTyping = (messages) => {
+    if (currentIndex < messages[currentIndex]?.text.length) {
+      setAllMessages(prevMessages => [...prevMessages, messages[currentIndex]?.text.slice(0, currentIndex + 1)]);
       setCurrentIndex(currentIndex + 1);
     }
   };
 
+  const addMessage = (message) => {
+    setAllMessages(prevMessages => [...prevMessages, message]);
+  };
+
   useEffect(() => {
-    setAi(aiData);
-    const typingInterval = setInterval(simulateTyping, typingSpeed);
+    setTextToType(aiData);
+    const typingInterval = setInterval(() => simulateTyping(textToType), typingSpeed);
 
     return () => {
       clearInterval(typingInterval);
     };
-  }, [currentIndex]);
+  }, [currentIndex, textToType]);
 
-  const handleInput = (text) => {
-    setUserInput(text);
+  const sendMess = () => {
+    const userMessage = input;
+    addMessage(userMessage);
 
-    // Process user input and update display text based on AI responses
-    if (count === 1) {
-      setCount(2);
-    } else if (count === 2) {
-      setCount(3);
-    } else if (count === 3) {
-      setCount(4);
-    } else if (count === 4) {
-      setCount(5);
-    }
+    // Simulate AI response (for demonstration purposes)
+    setTimeout(() => {
+      const aiResponse = `AI: I understand you're interested in "${userMessage}" genre films. Here are some recommendations.`;
+      addMessage(aiResponse);
+    }, 1000);
+
+    setInput('');
+    inputRef.current.clear();
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{displayText}</Text>
-      {count >= 2 && (
-        <Text style={styles.aiResponse}>{ai[count - 1].text}</Text>
-      )}
-      <TextInput
-        style={styles.input}
-        onChangeText={handleInput}
-        placeholder="Your answer..."
-        value={userInput}
-      />
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0} // Adjust as needed
+    >
+      <View style={styles.chatContainer}>
+        {allMessages.map((message, index) => (
+          <Text key={index} style={[styles.message, message.startsWith('AI:') && styles.aiMessage]}>
+            {message}
+          </Text>
+        ))}
+      </View>
+      <View style={styles.barSendMess}>
+        <TextInput
+          ref={inputRef}
+          style={styles.input}
+          placeholder="Which type of film"
+          placeholderTextColor="grey"
+          value={input}
+          onChangeText={setInput}
+        />
+        <TouchableOpacity style={styles.postButton} onPress={() => sendMess()}>
+          <Ionicons name="send" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#333',
+    padding: 20,
+    borderRadius: 14,
+    borderWidth: 1,
   },
-  text: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  aiResponse: {
-    marginTop: 10,
-    fontSize: 18,
-    color: 'gray',
-    textAlign: 'center',
+  chatContainer: {
+    flex: 1,
+    paddingBottom: 10,
   },
   input: {
-    marginTop: 20,
-    paddingHorizontal: 10,
-    width: 300,
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
+    color: 'white',
+    fontSize: 16,
+  },
+  postButton: {
+    marginLeft: 10,
+  },
+  message: {
+    fontSize: 18,
+    color: 'white',
+    marginBottom: 5,
+  },
+  barSendMess: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    backgroundColor: '#333',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  aiMessage: {
+    alignSelf: 'flex-start',
   },
 });
 
